1e9425e5f60ece79c889184de934578885376b45
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-02-16 15:26:53 -0300

    treinamento Desenvolvedor C# CRM primeiro commit

---
+++ Dynacoop2023.AlfaPeople.Dynamics365/Dynacoop2023.AlfaPeople.ConsoleApplication/Program.cs
@@ -0,0 +1,80 @@
+﻿using Dynacoop2023.AlfaPeople.ConsoleApplication.Controllers;
+using Dynacoop2023.AlfaPeople.ConsoleApplication.Models;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynacoop2023.AlfaPeople.ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CrmServiceClient serviceClient = Singleton.GetService();
+
+            ContaController contaController = new ContaController(serviceClient);
+
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("Digite 1 para Create/Update");
+            Console.WriteLine("Digite 2 para Delete");
+
+            var answerWhatToDo = Console.ReadLine();
+
+            if (answerWhatToDo.ToString() == "1")
+                MakeCreateAndUpdate(contaController);
+            else
+            {
+                if (answerWhatToDo.ToString() == "2")
+                {
+                    MakeDelete(contaController);
+                }
+                else 
+                { 
+                    Console.WriteLine("Opção inválida reinicie o app");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void MakeDelete(ContaController contaController)
+        {
+            Console.WriteLine("Digite o id da conta que voce quer deletar");
+            var accountId = Console.ReadLine();
+            contaController.Delete(new Guid(accountId));
+            Console.WriteLine("Conta deletada com sucesso");
+        }
+
+        private static void MakeCreateAndUpdate(ContaController contaController)
+        {
+            Console.WriteLine("Nova conta sendo criada");
+            Guid accountId = contaController.Create();
+            Console.WriteLine("Conta craida com sucesso");
+
+            Console.WriteLine($"https://org90caa978.crm2.dynamics.com/main.aspx?appid=4d306bb3-f4a9-ed11-9885-000d3a888f48&pagetype=entityrecord&etn=account&id={accountId}");
+
+
+            Console.WriteLine("Deseja atualizar a conta recém criada S/N");
+            var answerToUpdate = Console.ReadLine();
+
+            if (answerToUpdate.ToString().ToUpper() == "S")
+            {
+                Console.WriteLine("Por favor informe o novo telefone");
+                var newTelephone = Console.ReadLine();
+
+                bool contaAtualizada = contaController.Update(accountId, newTelephone);
+
+                if (contaAtualizada)
+                    Console.WriteLine("Conta atualizada com sucesso");
+                else
+                    Console.WriteLine("Erro ao atualizar conta");
+            }
+        }
+    }
+}
---
+++ Dynacoop2023.AlfaPeople.Dynamics365/Dynacoop2023.AlfaPeople.ConsoleApplication/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynacoop2023.AlfaPeople.ConsoleApplication
+{
+    public class Singleton
+    {
+        public static CrmServiceClient GetService()
+        {
+            string url = "org90caa978.crm2.dynamics.com/";
+            string clientId = "c1fa970e-36ff-4ca3-af66-02523e5b0b79";
+            string clientSecret = "iDt8Q~49_eoxOAG2jNxAaz~v9TqFpT4F7ZE~caiy";
+
+            CrmServiceClient serviceClient = new CrmServiceClient($"AuthType=ClientSecret;Url=https://{url}.crm2.dynamics.com/;AppId={clientId};ClientSecret={clientSecret};");
+
+            if (!serviceClient.CurrentAccessToken.Equals(null))
+                Console.WriteLine("Conexão realizada com sucesso");
+            else
+                Console.WriteLine("Erro na conexão");
+
+            return serviceClient;
+        }
+    }
+}
